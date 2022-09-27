a17c4fd40d4132ae5468526f0e4d5b050432733a
ayush0314 <f20212571@goa.bits-pilani.ac.in>
2022-09-27 22:50:21 +0530

    HTML conversion

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from markdown2 import Markdown
+from markdown2  import Markdown
 
 from . import util
 
+# def convert_md_html(title):
+#     content = util.get_entry(title)
+#     markdowner = Markdown.Markdown()
+#     if content == None:
+#         return None
+#     else:
+#         return markdowner.convert(content)
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
