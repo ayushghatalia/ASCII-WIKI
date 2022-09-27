131955039970fb8ab177f9f86049e0dee6a5c07d
ayush0314 <f20212571@goa.bits-pilani.ac.in>
2022-09-27 23:01:30 +0530

    checklast

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -4,7 +4,7 @@ from markdown2  import Markdown
 from . import util
 
 def convert_md_html(title):
-    # don't ignore
+    # don't ignore me
     content = util.get_entry(title)
     markdowner = Markdown.Markdown()
     if content == None:
